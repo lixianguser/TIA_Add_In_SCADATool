9693ac78389975480e00ca0e1b2f5b73d947ccd3
Sean李翔 <czlixiang@gmail.com>
2024-04-15 16:52:09 +0800

    添加项目文件。

---
+++ TIA_Add_In_SCADATool/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIA_Add_In_SCADATool
+{
+    public class Class1
+    {
+    }
+}
